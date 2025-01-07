0e823f07ab2de838e0ef66616e1e6da311421e1d
Shubham Giri <shubhamgiri7631.sg@gmail.com>
2025-01-07 13:54:03 +0530

    practice some hooks

--- state-management/src/components/StateOne.jsx
+++ state-management/src/components/StateOne.jsx
@@ -1,14 +1,28 @@
 import "../App.css";
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 const StateOne = () => {
   const [count, setCount] = useState(0);
+  const countRef = useRef(0);
+
+  //   useEffect(() => {
+  //     // the code we want to run
+  //     console.log("the cunt is:", count);
+  //   }, [count]); // the dependency arrey
+
+  const handleInc = () => {
+    setCount(count + 1);
+    countRef.current++;
+
+    console.log("state", count);
+    console.log("ref", countRef.current);
+  };
 
   return (
     <center>
       <h1>{count}</h1>
       <div className="buttons">
-        <button onClick={() => setCount(count + 1)}>increment</button>
+        <button onClick={handleInc}>increment</button>
         <button onClick={() => setCount(count - 1)}>decrement</button>
         <button onClick={() => setCount(0)}>reset</button>
       </div>
---
+++ state-management/src/components/context.js
@@ -0,0 +1,5 @@
+import { useContext } from "react";
+
+const DashboardContext = useContext(undefined);
+
+export default DashboardContext;
